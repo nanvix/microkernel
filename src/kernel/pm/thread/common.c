d3d59ab68c7644478eefd03fac029fa013380c0f
João Vicente Souto <joaovicentesouto@gmail.com>
2020-11-26 10:57:48 -0300

    [pm] Enhancement: Isolates more thread function on common file.

--- src/kernel/pm/thread/common.c
+++ src/kernel/pm/thread/common.c
@@ -27,7 +27,56 @@
 
 #include "common.h"
 
+/*
+ * Import definitions.
+ */
+EXTERN void kmain(int argc, const char *argv[]);
+
+/**
+ * @brief Thread table.
+ */
+EXTENSION PUBLIC struct thread threads[KTHREAD_MAX] = {
+	[0] = {
+		.resource = RESOURCE_STATIC_INITIALIZER,
+		.tid      = KTHREAD_MASTER_TID,
+		.coreid   = 0,
+		.state    = THREAD_RUNNING,
+		.arg      = NULL,
+		.start    = (void *) kmain,
+		.ctx      = NULL,
+	}
+};
+
+/**
+ * @brief Running Threads.
+ */
+EXTENSION PUBLIC struct thread * curr_threads[CORES_NUM];
+
 #if CLUSTER_IS_MULTICORE
+
+/**
+ * @brief Thread join conditions.
+ */
+EXTENSION PUBLIC struct condvar joincond[KTHREAD_MAX];
+
+/**
+ * @brief Number of running threads.
+ */
+PUBLIC int nthreads;
+
+/**
+ * @brief Next thread ID.
+ */
+PUBLIC int next_tid;
+
+/**
+ * @brief Thread manager lock.
+ */
+PUBLIC spinlock_t lock_tm;
+
+/*============================================================================*
+ * Return values                                                              *
+ *============================================================================*/
 
 /**
  * @brief Thread's exit value
@@ -36,17 +85,16 @@ EXTENSION PRIVATE struct exit_value
 {
 	int tid;
 	void * retval;
-} exit_values[KTHREAD_EXIT_VALUE_NUM] = {
-	[0 ... (KTHREAD_EXIT_VALUE_NUM - 1)] = {
-		.tid = KTHREAD_NULL_TID,
-		.retval = NULL
-	}
-};
+} retvals[KTHREAD_EXIT_VALUE_NUM];
 
 /**
  * @brief Global counter for saving exit values.
  */
-PRIVATE int next_exit_value = 0;
+PRIVATE int retval_curr_slot;
+
+/*============================================================================*
+ * thread_save_retval                                                         *
+ *============================================================================*/
 
 /**
  * @brief Saves the retval of the leaving thread in the exit values array.
@@ -59,15 +107,19 @@ PUBLIC void thread_save_retval(void* retval, struct thread *leaving_thread)
 	/* Check if retval is a valid pointer*/
 	if (retval)
 	{
-		exit_values[next_exit_value].tid    = thread_get_id(leaving_thread);
-		exit_values[next_exit_value].retval = retval;
+		retvals[retval_curr_slot].tid    = thread_get_id(leaving_thread);
+		retvals[retval_curr_slot].retval = retval;
 
-		next_exit_value = (next_exit_value + 1) % KTHREAD_EXIT_VALUE_NUM;
+		retval_curr_slot = (retval_curr_slot + 1) % KTHREAD_EXIT_VALUE_NUM;
 	}
 }
 
+/*============================================================================*
+ * thread_search_retval                                                       *
+ *============================================================================*/
+
 /**
- * @brief Searches the exit_values array for the target return value of
+ * @brief Searches the retvals array for the target return value of
  * a thread.
  *
  * @param retval return value of the target thread
@@ -88,13 +140,342 @@ PUBLIC void thread_search_retval(void **retval, int tid)
 		for (int i = 0; i < KTHREAD_EXIT_VALUE_NUM; i++)
 		{
 			/* Found? */
-			if (exit_values[i].tid == tid)
+			if (retvals[i].tid == tid)
 			{
-				*retval = exit_values[i].retval;
+				*retval = retvals[i].retval;
 				break;
 			}
 		}
 	}
 }
 
+/*============================================================================*
+ * Getters                                                                    *
+ *============================================================================*/
+
+/*============================================================================*
+ * thread_get_curr()                                                          *
+ *============================================================================*/
+
+/**
+* @brief Gets the currently running thread.
+*
+* The thread_get() function returns a pointer to the thread
+* that is running in the underlying core.
+*
+* @returns A pointer to the thread that is running in the
+* underlying core.
+*/
+PUBLIC struct thread * thread_get_curr(void)
+{
+	/* NULL pointer should not happen. */
+	return (curr_threads[core_get_id()]);
+}
+
+/*============================================================================*
+ * thread_get()                                                               *
+ *============================================================================*/
+
+/**
+ * @brief Returns the target thread.
+ *
+ * The thread_get() function performs a linear search in the table of
+ * threads for the thread whose ID equals to @p tid.
+ *
+ * @param tid ID of the target thread.
+ *
+ * @returns Upon successful completion, a pointer to the target thread
+ * is returned. Upon failure, a null pointed is returned instead.
+ *
+ * @note This function is NOT thread safe.
+ */
+PUBLIC struct thread * thread_get(int tid)
+{
+	/* Sanity check. */
+	KASSERT(tid > KTHREAD_NULL_TID);
+
+	/* Search for target thread. */
+	for (int i = 0; i < KTHREAD_MAX; i++)
+	{
+		/* Found. */
+		if (threads[i].tid == tid)
+			return (&threads[i]);
+	}
+
+	return (NULL);
+}
+
+/*============================================================================*
+ * Thread Allocation/Release                                                  *
+ *============================================================================*/
+
+/*============================================================================*
+ * thread_alloc()                                                             *
+ *============================================================================*/
+
+/**
+ * @brief Allocates a thread.
+ *
+ * The thread_alloc() function allocates a new thread structure in the
+ * table of threads.
+ *
+ * @returns Upon successful completion, a pointer to the newly
+ * allocated thread is returned. Upon failure, a NULL pointer is
+ * returned instead.
+ *
+ * @note This function is NOT thread-safe.
+ *
+ * @author Pedro Henrique Penna and João Vicente Souto
+ */
+PUBLIC struct thread * thread_alloc(void)
+{
+	for (int i = 1; i < KTHREAD_MAX; i++)
+	{
+		/* Verify the state of the thread. */
+		switch (threads[i].state)
+		{
+			/* Found a free thread. */
+			case THREAD_NOT_STARTED:
+				nthreads++;
+#if !CORE_SUPPORTS_MULTITHREADING
+				threads[i].coreid = i;
+				curr_threads[i]   = &threads[i];
+				break;
+#else
+				break;
+
+			/* Found a zombie thread (frees used kpages). */
+			case THREAD_ZOMBIE:
+				thread_free(&threads[i]);
+				break;
+
 #endif
+
+			/* Skip busy thread. */
+			default:
+				continue;
+		}
+
+		/* Initializes chosen thread. */
+		threads[i].state    = THREAD_STARTED;
+		threads[i].resource = RESOURCE_INITIALIZER;
+
+		return (&threads[i]);
+	}
+
+	/* All threads are in use. */
+	return (NULL);
+}
+
+/*============================================================================*
+ * thread_free()                                                              *
+ *============================================================================*/
+
+/**
+ * @brief Releases a thread.
+ *
+ * The thread_free() function releases the thread entry pointed to by
+ * @p t in the table of threads.
+ *
+ * @note This function is NOT thread-safe.
+ *
+ * @author Pedro Henrique Penna
+ */
+PUBLIC void thread_free(struct thread *t)
+{
+	KASSERT(WITHIN(t, &threads[0], &threads[KTHREAD_MAX]));
+
+#if CORE_SUPPORTS_MULTITHREADING
+
+	__thread_free(t);
+
+#else
+
+	curr_threads[t->coreid] = NULL;
+
+#endif
+
+	t->coreid = -1;
+	t->state  = THREAD_NOT_STARTED;
+	t->tid    = KTHREAD_NULL_TID;
+	nthreads--;
+}
+
+/*============================================================================*
+ * User Threads Responsibility                                                *
+ *============================================================================*/
+
+/*============================================================================*
+ * thread_start()                                                             *
+ *============================================================================*/
+
+/**
+ * @brief Starts a thread.
+ *
+ * The thread_start function is a wrapper routine for the user-level
+ * thread start routine. Overall, it does some basic, kernel level
+ * setup and calls the registered user-level function.
+ *
+ * @note This function does not return.
+ *
+ * @author Pedro Henrique Penna
+ */
+PUBLIC NORETURN void thread_start(void)
+{
+	void *retval;         /* Return value.   */
+	struct thread * curr; /* Current thread. */
+
+	curr = thread_get_curr();
+
+#if CORE_SUPPORTS_MULTITHREADING
+
+	spinlock_lock(&lock_tm);
+
+		/* Verifies if the next thread is zombie */
+		struct thread * zombie = (struct thread *) curr->resource.next;
+
+		if (zombie && zombie->state == THREAD_ZOMBIE)
+			thread_free(zombie);
+
+		curr->resource.next = NULL;
+
+	spinlock_unlock(&lock_tm);
+
+#endif
+
+	retval = curr->start(curr->arg);
+
+	thread_exit(retval);
+
+	/* Never gets here. */
+	UNREACHABLE();
+}
+
+/*============================================================================*
+ * thread_join()                                                              *
+ *============================================================================*/
+
+/**
+ * The thread_join() function causes the calling thread to block until
+ * the thread whose ID equals to @p tid its terminates execution, by
+ * calling thread_exit(). If @p retval is not a null pointed, then the
+ * return value of the terminated thread is stored in the location
+ * pointed to by @p retval.
+ *
+ * @retval -EINVAL Invalid thread ID.
+ *
+ * @see thread_exit().
+ *
+ * @todo Retrieve return value.
+ */
+PUBLIC int thread_join(int tid, void **retval)
+{
+	int ret;
+	struct thread *t;
+
+	/* Sanity check. */
+	KASSERT(tid > KTHREAD_NULL_TID);
+	KASSERT(tid != thread_get_curr_id());
+	KASSERT(tid != KTHREAD_MASTER_TID);
+
+	spinlock_lock(&lock_tm);
+
+		/* Wait for thread termination. */
+		if ((t = thread_get(tid)) != NULL)
+		{
+			/*
+			 * The target thread is still running,
+			 * so we have to block and wait for it.
+			 */
+			if (t->state != THREAD_NOT_STARTED &&
+				t->state != THREAD_TERMINATED &&
+				t->state != THREAD_ZOMBIE)
+				cond_wait(&joincond[KERNEL_THREAD_ID(t)], &lock_tm);
+		}
+
+		/**
+		 * Thread IDs are incremented by next_id. So, if we want to know
+		 * if the @p tid is valid and has already left, just check if it
+		 * is less than the next_tid.
+		 */
+		ret = (tid < next_tid) ? 0 : (-EINVAL);
+
+		/**
+		 * This prevents the thread from returning an invalid value.
+		 * This if is used guarante that the the @p ret is valid
+		 */
+		if (ret == 0)
+			thread_search_retval(retval, tid);
+
+	spinlock_unlock(&lock_tm);
+
+	return (ret);
+}
+
+#endif /* CLUSTER_IS_MULTICORE */
+
+/*============================================================================*
+ * Thread Manager Initialization                                              *
+ *============================================================================*/
+
+/*============================================================================*
+ * thread_init()                                                              *
+ *============================================================================*/
+
+/**
+ * @brief Initialize thread system.
+ */
+PUBLIC void thread_init(void)
+{
+	/**
+	 * Initializes global variables.
+	 */
+
+	curr_threads[0] = &threads[0];
+
+	/* Initializes user threads. */
+	for (int i = 1; i < KTHREAD_MAX; ++i)
+	{
+		threads[i].resource = RESOURCE_INITIALIZER;
+		threads[i].tid      = KTHREAD_NULL_TID;
+		threads[i].coreid   = -1;
+		threads[i].state    = THREAD_NOT_STARTED;
+		threads[i].arg      = NULL;
+		threads[i].start    = NULL;
+		threads[i].ctx      = NULL;
+	}
+
+	for (int i = 1; i < CORES_NUM; ++i)
+		curr_threads[i] = NULL;
+
+#if CLUSTER_IS_MULTICORE
+
+	/* Init join conditions. */
+	for (int i = 0; i < KTHREAD_MAX; ++i)
+		cond_init(&joincond[i]);
+
+	/** Init number of running threads. */
+	nthreads = 1;
+
+	/* Init next thread ID. */
+	next_tid = (KTHREAD_MASTER_TID + 1);
+
+	/* Init thread manager lock. */
+	spinlock_init(&lock_tm);
+
+	/* Init return values structure. */
+	for (int i = 0; i < KTHREAD_EXIT_VALUE_NUM; ++i)
+	{
+		retvals[i].tid = KTHREAD_NULL_TID;
+		retvals[i].retval = NULL;
+	}
+
+	/* Current slot on retval array. */
+	retval_curr_slot = 0;
+
+	/* Init dependent args. */
+	__thread_init();
+
+#endif /* CLUSTER_IS_MULTICORE */
+}
+
--- src/kernel/pm/thread/common.h
+++ src/kernel/pm/thread/common.h
@@ -21,9 +21,78 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include <nanvix/kernel/thread.h>
+
+#ifndef KERNEL_THREAD_COMMON_H_
+#define KERNEL_THREAD_COMMON_H_
+
+	#include <nanvix/kernel/thread.h>
+
+	/**
+	 * @brief Number of thread_create trials.
+	 */
+	#define THREAD_CREATE_NTRIALS 5
+
+	/**
+	 * @brief Kernel thread ID.
+	 */
+	#define KERNEL_THREAD_ID(_t) (_t - threads)
+
+	/**
+	 * @brief Thread table.
+	 */
+	EXTENSION EXTERN struct thread threads[KTHREAD_MAX];
+
+	/**
+	 * @brief Running Threads.
+	 */
+	EXTENSION EXTERN struct thread * curr_threads[CORES_NUM];
+
+	/**
+	 * @brief Returns the target thread.
+	 *
+	 * The thread_get() function performs a linear search in the table of
+	 * threads for the thread whose ID equals to @p tid.
+	 *
+	 * @param tid ID of the target thread.
+	 *
+	 * @returns Upon successful completion, a pointer to the target thread
+	 * is returned. Upon failure, a null pointed is returned instead.
+	 *
+	 * @note This function is NOT thread safe.
+	 */
+#if CLUSTER_IS_MULTICORE
+	EXTERN struct thread * thread_get(int tid);
+#else
+	static struct thread * thread_get(int tid)
+	{
+		if (tid != 0)
+			return (NULL);
+
+		return (KTHREAD_MASTER);
+	}
+#endif
 
 #if CLUSTER_IS_MULTICORE
+
+	/**
+	 * @brief Thread join conditions.
+	 */
+	EXTENSION EXTERN struct condvar joincond[KTHREAD_MAX];
+
+	/**
+	 * @brief Number of running threads.
+	 */
+	EXTERN int nthreads;
+
+	/**
+	 * @brief Next thread ID.
+	 */
+	EXTERN int next_tid;
+
+	/**
+	 * @brief Thread manager lock.
+	 */
+	EXTERN spinlock_t lock_tm;
 
 	/**
 	 * @brief Saves the retval of the leaving thread in the exit values array.
@@ -31,7 +100,7 @@
 	 * @param retval return value of the target thread
 	 * @param leaving_thread theard of the retval we're saving
 	 */
-    EXTERN void thread_save_retval(void* retval, struct thread *curr);
+    EXTERN void thread_save_retval(void *retval, struct thread *curr);
 
 	/**
 	 * @brief Searches the exit_values array for the target return value of
@@ -42,4 +111,67 @@
 	 */
     EXTERN void thread_search_retval(void **retval, int tid);
 
-#endif
+	/**
+	 * @brief Allocates a thread.
+	 *
+	 * The thread_alloc() function allocates a new thread structure in the
+	 * table of threads.
+	 *
+	 * @returns Upon successful completion, a pointer to the newly
+	 * allocated thread is returned. Upon failure, a NULL pointer is
+	 * returned instead.
+	 *
+	 * @note This function is NOT thread-safe.
+	 *
+	 * @author Pedro Henrique Penna and João Vicente Souto
+	 */
+	EXTERN struct thread * thread_alloc(void);
+
+	/**
+	 * @brief Releases a thread.
+	 *
+	 * The thread_free() function releases the thread entry pointed to by
+	 * @p t in the table of threads.
+	 *
+	 * @note This function is NOT thread-safe.
+	 *
+	 * @author Pedro Henrique Penna
+	 */
+	EXTERN void thread_free(struct thread *t);
+
+	/**
+	 * @brief Underlying releases a thread.
+	 *
+	 * The thread_free() function releases the thread entry pointed to by
+	 * @p t in the table of threads.
+	 *
+	 * @note This function is NOT thread-safe.
+	 *
+	 * @author João Vicente Souto
+	 */
+	EXTERN void __thread_free(struct thread *t);
+
+	/*============================================================================*
+	 * thread_start()                                                             *
+	 *============================================================================*/
+
+	/**
+	 * @brief Starts a thread.
+	 *
+	 * The thread_start function is a wrapper routine for the user-level
+	 * thread start routine. Overall, it does some basic, kernel level
+	 * setup and calls the registered user-level function.
+	 *
+	 * @note This function does not return.
+	 *
+	 * @author Pedro Henrique Penna
+	 */
+	EXTERN NORETURN void thread_start(void);
+
+	/**
+	 * @brief Initialize thread system.
+	 */
+	EXTERN void __thread_init(void);
+
+#endif /* CLUSTER_IS_MULTICORE */
+#endif /* KERNEL_THREAD_COMMON_H_ */
